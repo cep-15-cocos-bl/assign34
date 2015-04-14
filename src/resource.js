49f6d1529e6119a253e054845402666f3fa26897
Lhwhatever <llinhong.2301@gmail.com>
2015-04-14 16:26:11 +0800

    Remove extraneous files

--- src/resource.js
+++ src/resource.js
@@ -1,11 +1,5 @@
 var res = {
-    player_png : "res/player.png",
-    ground_png : "res/ground.png",
-    platform_png: "res/platform.png",
-    jump_png: "res/jumping.png",
-    jump_plist: "res/jumping.plist",
-    walk_png:"res/walking.png",
-    walk_plist:"res/walking.plist"
+
 };
 
 var g_resources = [];
