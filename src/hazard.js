4115e4efe90faa38605c71e8e010b5d2e24bc451
Lhwhatever <llinhong.2301@gmail.com>
2015-05-01 20:34:02 +0800

    Implement hazard

---
+++ src/hazard.js
@@ -0,0 +1,28 @@
+var HazardClass = cc.Sprite.extend({
+    name: "hazard",
+    world: null,
+    pbody: null,
+    pshape: null,
+
+    ctor: function(game, gWorld, posX, posY, id) {
+
+        // hard-coded
+        width = 80;
+        height = 80;
+
+        this.world = gWorld;
+        this.world.hazards[id] = this;
+
+        this.pbody = new cp.Body(4, Infinity);
+        this.pbody.addBody(this.pbody);
+
+        this.pshape = this.world.addShape(this.pbody, width, height);
+        this.pshape.setFriction(5);
+        this.pshape.setElasticity(0.0);
+        this.pshape.name = "hazard" + id;
+
+        this.pshape.setCollisionType("square");
+
+
+    }
+});
