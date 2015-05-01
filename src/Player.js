ac5f7f32b613e9a8a78f949bb66d0c4eaddec90e
Lhwhatever <llinhong.2301@gmail.com>
2015-05-01 22:47:58 +0800

    Basic implementation for player animation

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,6 @@
 var PlayerClass = cc.Sprite.extend({
     name: "player",
+    game: null,
     world: null,
     pbody: null,
     pshape: null,
@@ -11,12 +12,49 @@ var PlayerClass = cc.Sprite.extend({
 
     ctor: function(game, gWorld, posX, posY) {
 
+        this.game = game;
+        this.world = gWorld;
+        this.world.player = this;
+
+        cc.spriteFrameCache.addSpriteFrames(res.walk_plist);
+        this.spriteSheet = new cc.SpriteBatchNode(res.walk_png);
+        this.addChild(this.spriteSheet);
+        cc.spriteFrameCache.addSpriteFrames(res.jump_plist);
+        this.jspriteSheet = new cc.SpriteBatchNode(res.jump_png);
+        this.addChild(this.jspriteSheet);
+
+        var walkFrames = [];
+        var jumpFrames = [];
+
+        for (var i = 1; i < 5; i++) {
+            var str = "walk" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(str);
+            walkFrames.push(frame);
+        }
+        for (var j = 1; j < 5; j++) {
+            var jstr = "jump" + j + ".png";
+            var jframe = cc.spriteFrameCache.getSpriteFrame(jstr);
+            jumpFrames.push(jframe);
+        }
+
+        var animation = new cc.Animation(walkFrames, 0.5);
+        var janimation = new cc.Animation(jumpFrames, 0.2);
+
+        this.walkAction = new cc.RepeatForever(new cc.Animate(animation));
+        this.jumpAction = new cc.Repeat(new cc.Animate(janimation),1);
+        this.playerSprite = new cc.Sprite.create(spriteImage);
+
+        this.game.addChild(playerSprite,0);
+        this.playerSprite.setPosition(posX,posY);
+        var spriteImage = res.player_png;
+        this.playerSprite.runAction(this.walkAction);
+        this.spriteSheet.addChild(this.playerSprite);
+        this.fixBody(game,gWorld,posX,posY,width,height,isDynamic,spriteImage);
+        return this;
+
         // hard-coded
         width = 25;
         height = 25;
-
-        this.world = gWorld;
-        this.world.player = this;
 
         this.pbody = new cp.Body(1, Infinity);
         this.pbody.setPos(cp.v(posX, posY));
@@ -27,10 +65,10 @@ var PlayerClass = cc.Sprite.extend({
         this.pshape.setElasticity(0.0);
         this.pshape.name = "player";
         this.pshape.id = 0;
+        this.pshape.image = this.playerSprite;
         this.pshape.type = "player";
 
         this.pshape.setCollisionType("player");
-
 
     },
 
@@ -43,9 +81,13 @@ var PlayerClass = cc.Sprite.extend({
         this.canJump = false;
 
         if(compare < this.pbody.getPos().x + 10) { // jump left
+            playerSprite.runAction(this.jumpAction);
+            this.jspriteSheet.addChild(playerSprite); 
             this.pbody.applyImpulse(cp.v(-80, 60), cp.v(40, 80));
             return 3;
         } else if(compare > this.pbody.getPos().x + 10) { // jump right
+            playerSprite.runAction(this.jumpAction);
+            this.jspriteSheet.addChild(playerSprite); 
             this.pbody.applyImpulse(cp.v(80, 60), cp.v(-40, 80));
             return 2;
         } else {
